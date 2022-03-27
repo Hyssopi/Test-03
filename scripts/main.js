84f63617798b6d85260c88df51952b969d1ea317
Hyssopi <43843070+Hyssopi@users.noreply.github.com>
2022-03-26 23:01:29 -0700

    Test

--- scripts/main.js
+++ scripts/main.js
@@ -40,6 +40,9 @@ function loadData(dataPath)
       const lists = Array.prototype.slice.call(document.querySelectorAll('ul'));
       for (const list of lists)
       {
+        list.addEventListener('click', listCallback);
+        list.addEventListener('touchstart', listCallback);
+        /*
         list.addEventListener('click', function(e)
         {
           if (e.target.tagName === 'LI')
@@ -47,6 +50,7 @@ function loadData(dataPath)
             e.target.classList.toggle('checked');
           }
         }, false);
+        */
       };
 
 
@@ -59,6 +63,9 @@ function loadData(dataPath)
       const accordions = document.getElementsByClassName('accordion');
       for (const accordion of accordions)
       {
+        accordion.addEventListener('click', accordionCallback);
+        accordion.addEventListener('touchstart', accordionCallback);
+        /*
         accordion.addEventListener('click', function()
         {
           this.classList.toggle('active');
@@ -72,6 +79,7 @@ function loadData(dataPath)
             panel.style.maxHeight = panel.scrollHeight + 'px';
           }
         });
+        */
       }
     })
     .catch (function(error)
@@ -80,7 +88,27 @@ function loadData(dataPath)
     })
 }
 
+function listCallback(event)
+{
+  if (e.target.tagName === 'LI')
+  {
+    e.target.classList.toggle('checked');
+  }
+}
 
+function accordionCallback(event)
+{
+  this.classList.toggle('active');
+  const panel = this.nextElementSibling;
+  if (panel.style.maxHeight)
+  {
+    panel.style.maxHeight = null;
+  }
+  else
+  {
+    panel.style.maxHeight = panel.scrollHeight + 'px';
+  }
+}
 
 
 /**
